39f20a2e367d2fd430824d4c0d878d5cc0cdb737
Anmol Tyagi <anmoltyagi908@gmail.com>
2025-01-09 19:34:08 +0530

    add port

--- server/server.js
+++ server/server.js
@@ -132,7 +132,7 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
