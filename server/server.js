4ed15002dd6c0b6cedd6ccf291a8df31fc1c9962
Anmol Tyagi <anmoltyagi908@gmail.com>
2025-01-09 18:36:04 +0530

    add some depen

--- server/server.js
+++ server/server.js
@@ -132,10 +132,10 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json());
-
-// API Routes
-app.use("/api/session", sessionRoutes);
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// API Routes
+app.use("/api/session", sessionRoutes);
+
